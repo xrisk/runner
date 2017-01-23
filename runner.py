d848852e877a0ea65fb9f62a968ca1042ecea37c
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-23 22:06:47 +0530

    as

--- runner.py
+++ runner.py
@@ -4,6 +4,10 @@ import subprocess
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET'])
+def run():
+    return "Hello world"
+
 @app.route("/", methods=['POST', 'PUT'])
 def run():
     f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.STDOUT)
