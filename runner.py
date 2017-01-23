bd2b56929c50bfd9c617f306e4841d128426e610
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-23 21:37:21 +0530

    aa

--- runner.py
+++ runner.py
@@ -9,5 +9,4 @@ def run():
     f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.STDOUT)
     return (f.stdout, {'Access-Control-Allow-Origin': '*'})
 
-if __name__ == "__main__":
-    app.run(port=int(os.getenv("PORT", 5000)))
+app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
