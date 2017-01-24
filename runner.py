7de0c00b47ca71b94b62fc092c35c17d916c41cf
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-24 17:52:12 +0530

    Add c++

--- runner.py
+++ runner.py
@@ -1,16 +1,32 @@
 from flask import Flask, request
 import os
+
 import subprocess
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def foo():
-    return "Hello world"
+@app.route('/c++', methods=['POST', 'PUT'])
+def cpp():
+    code = request.form["code"]
+    name = str(hash(code))
 
-@app.route("/", methods=['POST', 'PUT'])
-def run():
+    f = subprocess.run(["g++", "-xc++", "-o", name, "-"], input=code, stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.PIPE)
+    if f.returncode != 0:
+        return (f.stderr, {'Access-Control-Allow-Origin': '*'})
+    else:
+        g = subprocess.run(["./{}".format(name)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        return (g.stdout, {'Access-Control-Allow-Origin': '*'})
+
+@app.route("/python", methods=['POST', 'PUT'])
+def python():
     f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.STDOUT)
-    return (f.stdout, {'Access-Control-Allow-Origin': '*'})
+    if f.returncode != 0:
+        return (f.stderr, {'Access-Control-Allow-Origin': '*'})
+    else:
+        return (f.stdout, {'Access-Control-Allow-Origin': '*'})
+
+@app.route("/", methods=['GET'])
+def help():
+    return "http[ie] -f POST /lang-id/ code=@input.py"
 
 app.run(host='0.0.0.0', port=int(os.getenv("PORT")))
