8b719238b5d9b37ab0d1ce4ee07b69c1da497a10
Rishav Kundu <rishav.kundu98@gmail.com>
2017-02-17 21:40:55 +0530

    Refactor

--- librepad/setup.py
+++ librepad/setup.py
@@ -1,3 +1,15 @@
 from distutils.core import setup
 
-setup(name='librepad', version='1.0', py_modules=['librepad'])
+setup(
+    name='librepad',
+    version='1.0',
+    author='Rishav Kundu',
+    author_email='rishav.kundu98@gmail.com',
+    url='https://rishav.js.org/librepad',
+    license='MIT',
+    description='Server code for librepad',
+    py_modules=['librepad'],
+    install_requires=[
+          "Flask>=0.12",
+      ],
+)
