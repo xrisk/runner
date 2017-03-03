eec158b7f543b952cef29c3be6b4d5cfc6140a1f
Rishav Kundu <rishav.kundu98@gmail.com>
2017-03-03 21:23:27 +0530

    Sandbox, JS, Bash

--- librepad/librepad.py
+++ librepad/librepad.py
@@ -1,30 +1,64 @@
 #!/usr/bin/env python3
 from flask import Flask, request
-import os
 
 import subprocess
 
 app = Flask(__name__)
+default = ["firejail", "--net=none", "--quiet", "--private=/sandbox"]
+
 
 @app.route('/c++', methods=['POST', 'PUT'])
 def cpp():
-    code = request.form["code"]
-    name = str(hash(code))
-
-    f = subprocess.run(["g++", "-xc++", "-o", name, "-"], input=code, stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.PIPE)
+    f = subprocess.run(default + ["g++", "-xc++", "-"],
+                       input=request.form["code"],
+                       stdout=subprocess.PIPE,
+                       universal_newlines=True,
+                       stderr=subprocess.PIPE)
     if f.returncode != 0:
         return (f.stderr, {'Access-Control-Allow-Origin': '*'})
     else:
-        g = subprocess.run(["./{}".format(name)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        g = subprocess.run(default + ["./a.out"],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
         return (g.stdout, {'Access-Control-Allow-Origin': '*'})
+
 
 @app.route("/python", methods=['POST', 'PUT'])
 def python():
-    f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.STDOUT)  
+    f = subprocess.run(default + ["python3"],
+                       input=request.form["code"],
+                       stdout=subprocess.PIPE,
+                       universal_newlines=True,
+                       stderr=subprocess.STDOUT)
     return (f.stdout, {'Access-Control-Allow-Origin': '*'})
+
+
+@app.route('/js', methods=['POST', 'PUT'])
+def node():
+    f = subprocess.run(default + ["nodejs"],
+                       input=request.form["code"],
+                       stdout=subprocess.PIPE,
+                       universal_newlines=True,
+                       stderr=subprocess.STDOUT)
+    return (f.stdout, {'Access-Control-Allow-Origin': '*'})
+
+
+@app.route('/bash', methods=['POST', "PUT"])
+def bash():
+    f = subprocess.run(default + ["bash", "--"],
+                       input=request.form["code"],
+                       stdout=subprocess.PIPE,
+                       universal_newlines=True,
+                       stderr=subprocess.STDOUT)
+    return (f.stdout, {'Access-Control-Allow-Origin': '*'})
+
 
 @app.route("/", methods=['GET'])
 def help():
     return "http[ie] -f POST /lang-id/ code=@input.py"
 
+
 application = app
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", "8000")
